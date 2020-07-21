2e9dff9cd5ef3795b047f6ea1c8646241d3eaa6c
Chuck Sigo <yardgnomeninja@gmail.com>
2020-07-20 21:15:36 -0400

    add readme and comments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Useful when not using a Rigidbody
     CharacterController characterController;
     public float speed = 5f;
     private float gravity = -9.81f;
@@ -16,33 +17,45 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
+    // Note: For resons I don't yet comprehend, moving the gravity calculations below movement causes undesireable effects for isGrounded. Hence they happen first.
     void Update()
     {
+        // Calculate Gravity
         if(characterController.isGrounded) {
+            // If the character is on the ground, continue to apply downward force to keep them there
             playerVelocity.y = gravity * Time.deltaTime;
 
+            // Only allow the character to jump if they are on the ground
             if(Input.GetKeyDown(KeyCode.Space)) {
+                // If the player presses space, overcome gravity to jump
                 playerVelocity.y = 5f;
             }
         }
         else
         {
+            // If the character is not on the ground, increase the force of gravity gradually
             playerVelocity.y += gravity * Time.deltaTime;
         }
         
+        // Get movement input
         var horizontalInput = Input.GetAxis("Horizontal");
         var verticalInput = Input.GetAxis("Vertical");
 
         playerVelocity.x = horizontalInput;
         playerVelocity.z = verticalInput;
 
+        // Store the character's movement for multiple uses
         var characterMovement = new Vector3(playerVelocity.x, 0, playerVelocity.z);
+
+        // Move the character
         characterController.Move(characterMovement * speed * Time.deltaTime);
 
+        // If the character moved, face them in the appropriate direction
         if(characterMovement != Vector3.zero) {
             transform.forward = characterMovement;
         }
 
+        // Apply gravity
         characterController.Move(new Vector3(0, playerVelocity.y, 0) * Time.deltaTime);
     }
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+Created using a combination of these videos
+    https://www.youtube.com/watch?v=miMCu5796KM
+    https://www.youtube.com/watch?v=-nS1gqSk458
